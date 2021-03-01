c4bc47ac230ca59f514eb3e71bb969c41cae629c
Shahriar Swim <shahriar2k18@gmail.com>
2021-03-01 23:29:31 +0600

    added active link animation

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,12 +7,12 @@ import contents from "../contents/index";
 const Header = () => {
   const { header } = contents;
   return (
-    <header className="flex justify-between align-center px-20 py-7 md:py-8 bg-purple-800 text-white">
-      <h1 className="font-bold text-3xl">
+    <header className="flex flex-col sm:flex-row justify-between align-center  px-20 py-4 md:py-6 bg-purple-800 text-white">
+      <h1 className="font-bold text-2xl md:text-3xl mb-2 sm:mb-0 text-center">
         <NavLink to="/">{header.logo}</NavLink>
       </h1>
       <nav>
-        <ul className="flex justify-center align-center text-lg">
+        <ul className="flex justify-center align-center text-md md:text-lg">
           {header.nav.map((navItem, index) => (
             <li
               key={index}
@@ -20,7 +20,7 @@ const Header = () => {
             >
               <NavLink
                 to={navItem.link}
-                className="p-1 hover:animate-bounce"
+                className="p-1 transition-all duration-200 delay-100 ease-in"
                 exact
                 activeClassName="border-b-4 border-white"
               >
