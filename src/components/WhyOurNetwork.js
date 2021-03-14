f74d934c7ca008b3b5b7063cdc1b1ad0d130cb8a
Shahriar Swim <shahriar2k18@gmail.com>
2021-03-15 00:03:02 +0600

    text fix to whyOurNetwork

--- src/components/WhyOurNetwork.js
+++ src/components/WhyOurNetwork.js
@@ -3,13 +3,13 @@ import React from "react";
 const WhyOurNetwork = ({ contents }) => {
   return (
     <div className="py-28 px-10 md:px-20 dark:bg-gray-800 ">
-      <h2 className="text-center font-bold text-4xl text-gray-700 dark:text-gray-100 uppercase">
+      <h2 className="text-center font-bold text-3xl lg:text-4xl text-gray-700 dark:text-gray-100 uppercase">
         {contents.title}
       </h2>
       <div className="flex justify-center md:justify-between flex-wrap mt-16 ">
         {contents.features.map((content, index) => {
           return (
-            <div className="w-60 lg:w-70 text-center" key={index}>
+            <div className="w-60 lg:w-70 text-center m-5 lg:m-0" key={index}>
               <button className="focus:outline-none text-6xl text-gray-100 bg-gray-800 dark:bg-purple-700 p-5 rounded-full shadow-lg">
                 {content.icon}
               </button>
