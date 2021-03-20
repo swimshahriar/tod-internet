4dffb8f138fa5a0e4de8a91e838d00085b7a095d
Shahriar Swim <shahriar2k18@gmail.com>
2021-03-20 23:15:35 +0600

    structured packages area

--- src/components/Package.js
+++ src/components/Package.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Package = ({ packageInfo, fav }) => {
   return (
-    <div>
+    <div className="bg-gray-700 p-5 text-white m-5">
       <h2>{packageInfo.name}</h2>
       <h4>{packageInfo.price} TK/month</h4>
       <div>
--- src/components/Packages.js
+++ src/components/Packages.js
@@ -1,23 +1,25 @@
-import React from 'react';
+import React from "react";
 
 // components
-import Package from './Package';
+import Package from "./Package";
 
 const Packages = ({ contents }) => {
   return (
-    <div className="px-10 md:px-20">
-      <h2 className="text-center font-bold text-3xl lg:text-4xl">
+    <div className="px-10 md:px-20 h-96 my-5">
+      <h2 className="text-center font-bold text-3xl lg:text-4xl text-gray-700 dark:text-gray-100 uppercase">
         {contents.title}
       </h2>
-      {contents.info.map((packageInfo, index) => {
-        return (
-          <Package
-            packageInfo={packageInfo}
-            fav={packageInfo.fav}
-            key={index}
-          />
-        );
-      })}
+      <div className="flex flex-wrap justify-around m-5">
+        {contents.info.map((packageInfo, index) => {
+          return (
+            <Package
+              packageInfo={packageInfo}
+              fav={packageInfo.fav}
+              key={index}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 };
