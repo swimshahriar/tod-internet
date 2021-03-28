b92afa23a8bb5e4223454177cf5a00d89de084af
Shahriar Swim <shahriar2k18@gmail.com>
2021-03-28 23:57:56 +0600

    fix dark mode services section

--- src/components/Service.js
+++ src/components/Service.js
@@ -2,8 +2,8 @@ import React from "react";
 
 const Service = ({ icon, desc }) => {
   return (
-    <div className="flex justify-center align-middle m-5 h-32 w-72 shadow-md border-t-4 rounded-t-md border-purple-700 hover:bg-gray-100 cursor-pointer">
-      <button className="text-4xl mr-3">{icon}</button>
+    <div className="flex justify-center align-middle m-5 h-32 w-72 shadow-md border-t-4 rounded-t-md border-purple-700 dark:bg-gray-100 dark:hover:bg-gray-200 hover:bg-gray-100 cursor-pointer">
+      <button className="text-4xl mr-3 focus:outline-none">{icon}</button>
       <p className="mr-3 my-auto text-gray-700">{desc}</p>
     </div>
   );
