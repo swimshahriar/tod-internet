9d1be1c3d6f5612b9ec5e9e80953f71d8a97694b
Shahriar Swim <shahriar2k18@gmail.com>
2021-03-28 23:47:25 +0600

    redesign services cards

--- src/components/Service.js
+++ src/components/Service.js
@@ -2,9 +2,9 @@ import React from "react";
 
 const Service = ({ icon, desc }) => {
   return (
-    <div className="flex justify-center align-middle">
+    <div className="flex justify-center align-middle m-5 h-32 w-72 shadow-md border-t-4 rounded-t-md border-purple-700 hover:bg-gray-100 cursor-pointer">
       <button className="text-4xl mr-3">{icon}</button>
-      <p className="mr-3 text-gray-700">{desc}</p>
+      <p className="mr-3 my-auto text-gray-700">{desc}</p>
     </div>
   );
 };
