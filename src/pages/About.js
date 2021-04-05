49c99953497733c2cead5aa421067708a48990da
Shahriar Swim <shahriar2k18@gmail.com>
2021-04-05 23:12:10 +0600

    about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,8 +2,27 @@ import React from "react";
 
 const About = () => {
   return (
-    <div>
-      <h1>About Page</h1>
+    <div className="px-10 md:px-20 py-5 dark:bg-gray-800 dark:text-gray-100">
+      <h1 className="text-center p-5 text-3xl font-bold uppercase">About</h1>
+      <p className="text-xl p-7">
+        TOD Internet has started its operation in the year 2005 with the
+        commitment of quality internet service at an affordable price. At the
+        time of commencement TOD Internet started with broadband service with
+        the state of art technology. In the course of time it’s R & D team has
+        developed and introduced many new technologies to meet the requirement
+        of new millennium. In the last 15 years TOD Internet has introduced many
+        new ideas and technologies to the internet industry.
+        <br />
+        We beieve we will be successful if our clients are successful. Solving
+        the hardest problems requires the best people. We think that the best
+        people will be drawn to the opportunity to work on the hardest problems.
+        We have built our company around that belief.
+        <br />
+        One of our main Aim is to satisfy all the Gamers out there, by providing
+        them with stable Internet Connection along with Real IP and port
+        blocking/forwarding solutions, good stable pings in European and
+        Singapore servers.
+      </p>
     </div>
   );
 };
