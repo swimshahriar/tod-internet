f8d63b8ebf9e7fdcd4327e1e326573c222e6bea5
Shahriar Swim <shahriar2k18@gmail.com>
2021-05-10 23:43:34 +0600

    contact form & office info

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react";
 import mapboxgl from "mapbox-gl/dist/mapbox-gl";
+import { AiFillHome, AiFillPhone, AiFillMail } from "react-icons/ai";
 
 // styles
 import "mapbox-gl/dist/mapbox-gl.css";
@@ -24,29 +25,39 @@ const Contact = () => {
 
   return (
     <div className="px-10 md:px-20 py-5 dark:bg-gray-800 dark:text-gray-100">
-      <h1 className="text-center text-2xl font-bold mt-4 mb-6">Conatct Page</h1>
+      <h1 className="text-center text-3xl font-bold mt-4 mb-6">Contact Page</h1>
       <div id="map-container" style={{ height: "30rem" }}></div>
-      <div className="mt-6 flex">
-        <form className="form">
+      <div className="mt-6 flex md:flex-nowrap flex-wrap justify-center align-middle">
+        <form className="form w-96" onSubmit={(e) => e.preventDefault()}>
           <label htmlFor="name">Name:</label> <br />
-          <input type="text" id="name" className="" />
+          <input type="text" id="name" className="w-2/3" />
           <br />
           <label htmlFor="email">Email:</label> <br />
-          <input type="email" id="email" className="" />
+          <input type="email" id="email" className="w-2/3" />
           <br />
           <label htmlFor="subject">Subject:</label> <br />
-          <input type="text" id="subject" className="" />
+          <input type="text" id="subject" className="w-2/3" />
           <br />
           <label htmlFor="message">Message:</label> <br />
-          <textarea row="3" col="5" id="message" className="" />
+          <textarea row="3" col="5" id="message" className="w-2/3" />
           <br />
+          <button className="p-2 mt-2 dark:bg-purple-700 bg-gray-700 dark:hover:bg-purple-800 hover:bg-gray-800 text-gray-100 text-xl font-bold uppercase rounded">
+            send
+          </button>
         </form>
-        <div>
-          <h3>Head Office</h3>
-          <div>
-            <p>Address:</p>
-            <p>Phone:</p>
-            <p>Email:</p>
+        <div className="w-96">
+          <h3 className="font-bold text-xl">Head Office</h3>
+          <div className="mt-3">
+            <p className="flex justify-start align-middle mb-2">
+              <AiFillHome className="mr-3" />
+              Bashundhara R/A, Dhaka
+            </p>
+            <p className="flex justify-start align-middle mb-2">
+              <AiFillPhone className="mr-3" /> +8801777111111
+            </p>
+            <p className="flex justify-start align-middle">
+              <AiFillMail className="mr-3" /> admin@tod.com
+            </p>
           </div>
         </div>
       </div>
